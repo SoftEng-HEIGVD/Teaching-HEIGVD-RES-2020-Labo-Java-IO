b03c683121c96f05adb425d3ac850b5a4fb50e68
jerome arn <jerome.arn@heig-vd.ch>
2020-02-28 17:52:48 +0100

    Uppercassefilter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -16,17 +16,29 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < len; i++){
+      if((int)str.charAt(i) >= 97 || (int)str.charAt(i) <= 122)
+        write( (char) ((int)str.charAt(i) - 32));
+    }
+    //throw new UnsupportedOperationException("UpperCaseFilterWriter 1 ");
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < len; i++){
+      if((int)cbuf[i] >= 97 || (int)cbuf[i] <= 122)
+        write ((char)(cbuf[i] - 32));
+    }
+    //throw new UnsupportedOperationException("UpperCaseFilterWriter 2");
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(c >= 97 || c <= 122){
+      c = c -32;
+      write((char)c);
+    }
+    //throw new UnsupportedOperationException("UpperCaseFilterWriter 3");
   }
 
 }
