a4c2723c876353ab2973c59242ff8a167f2f46ac
Gil Balsiger <gil.balsiger@gmail.com>
2020-02-28 16:53:11 +0100

    Fix FileNumberingFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -10,32 +10,79 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * <p>
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int counter = 0;
+    private boolean isFirst = true;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        if (off < 0 || len < 1 || off > str.length() || len > str.length() || off + len > str.length()) {
+            throw new IOException("Offset or length invalid");
+        } else {
+            StringBuilder buffer = new StringBuilder();
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+            if(isFirst) {
+                buffer.append(++counter);
+                buffer.append('\t');
+                isFirst = false;
+            }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+            for (int i = off; i < off + len; i++) {
+                buffer.append(str.charAt(i));
+                if (str.charAt(i) == '\n' || (str.charAt(i) == '\r' && str.charAt(i+1) != '\n')) {
+                    buffer.append(++counter);
+                    buffer.append('\t');
+                }
+            }
+            out.write(buffer.toString());
+        }
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        if (off < 0 || len < 1 || off > cbuf.length || len > cbuf.length || off + len > cbuf.length) {
+            throw new IOException("Offset or length invalid");
+        } else {
+            StringBuilder buffer = new StringBuilder();
+
+            if(isFirst) {
+                buffer.append(++counter);
+                buffer.append('\t');
+                isFirst = false;
+            }
+
+            for (int i = off; i < off + len; i++) {
+                buffer.append(cbuf[i]);
+                if (cbuf[i] == '\n') {
+                    buffer.append(++counter);
+                    buffer.append('\t');
+                }
+            }
+            out.write(buffer.toString());
+        }
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        if ((char) c == '\n') {
+            out.write('\n' + Integer.toString(++counter) + '\t');
+        } else if (isFirst) {
+            out.write(Integer.toString(++counter) + '\t' + (char) c);
+            isFirst = false;
+        } else {
+            out.write((char) c);
+        }
+    }
 
 }
