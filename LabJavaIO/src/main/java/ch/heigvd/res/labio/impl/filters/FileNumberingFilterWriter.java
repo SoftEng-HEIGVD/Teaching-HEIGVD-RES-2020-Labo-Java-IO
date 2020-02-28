0695d59056b8d6a922afd6eac9ec8661f9744f95
LucasGianinetti <lucas.gianinetti@heig-vd.ch>
2020-02-28 18:22:08 +0100

    FileNumberingTests pass but itShouldHandleWriteWithAnInt() fails

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,24 +18,56 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-
+  private int counter = 1;
+  private boolean isFirstLine = true;
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
+  //Return a string with line number and tab character after new line chars
+  private String fileNumbering(String str, int off, int len){
+    StringBuilder transformedStr = new StringBuilder();
+    if(isFirstLine){
+      transformedStr.append(counter + "\t");
+      isFirstLine = false;
+    }
+    if(str.contains("\n")){//Unix or Windows
+      for (int i = off; i < off + len; ++i) {
+        transformedStr.append(str.charAt(i));
+        if (str.charAt(i) == '\n') {
+          counter++;
+          transformedStr.append(counter + "\t");
+        }
+      }
+
+    }else{ //IOS9
+      for (int i = off; i < off + len; ++i) {
+        transformedStr.append(str.charAt(i));
+        if(str.charAt(i) == '\r'){
+          counter++;
+          transformedStr.append(counter + "\t");
+        }
+      }
+    }
+
+    return transformedStr.toString();
+  }
+
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.out.write(fileNumbering(str,off,len));
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String str = String.valueOf(cbuf);
+    this.out.write(fileNumbering(str,off,len));
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String str = String.valueOf((char)c);
+    this.out.write(fileNumbering(str,0,1));
   }
 
 }
