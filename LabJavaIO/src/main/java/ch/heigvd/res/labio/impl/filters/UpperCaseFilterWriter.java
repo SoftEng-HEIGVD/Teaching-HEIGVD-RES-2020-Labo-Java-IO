aef7d4bf455115e081265cd6d615f71e6ebc2622
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-02-29 11:20:51 +0100

    Implement the UpperCaseFilterWriter
    We make use of the String methods to transform a sequence of characters
    to their uppercase equivalent.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -16,17 +16,17 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(str.toUpperCase(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(new String(cbuf, off, len).toUpperCase(), 0, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    super.write(Character.toUpperCase(c));
   }
 
 }
