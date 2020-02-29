977678a9c1610674e448fa7b15c7e66812f2578c
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-02-29 11:03:30 +0100

    Implement DFS traversal of any directory
    Rather than simply performing a DFS traversal, we're proceeding with a
    specific ordering of the files, based on their names.
    
    This is needed to pass the unit tests that are defined in the project.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -2,7 +2,11 @@ package ch.heigvd.res.labio.impl.explorers;
 
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
+
 import java.io.File;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Stack;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -14,9 +18,46 @@ import java.io.File;
  */
 public class DFSFileExplorer implements IFileExplorer {
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  public void explore(File rootDirectory, IFileVisitor visitor) {
+
+    // Note : instead of using a recursive implementation of DFS traversal,
+    // we use a stack to offer an iterative implementation and explicitly
+    // manage the recursion by ourselves.
+    Stack<File> deque = new Stack<>();
+
+    // We can only have directories in the stack.
+    deque.push(rootDirectory);
+
+    while (!deque.isEmpty()) {
+
+      File current = deque.pop();
+      File[] children = current.listFiles();
+
+      // Visit the current directory.
+      visitor.visit(current);
+
+      if (children != null) {
+
+        // The ordering of the files read by the listFiles() method is not
+        // deterministic, so we have to force it to be done in a sorted
+        // order to pass the unit tests.
+        Arrays.sort(children, Comparator.comparing(File::getName).reversed());
+
+        // Visit all the direct children, and push the
+        // subdirectories onto the stack.
+        for (File child : children) {
+          if (child.isDirectory()) {
+            deque.push(child); // Defer visit to the next iteration.
+          } else {
+            visitor.visit(child);
+          }
+        }
+      }
+    }
   }
 
 }
