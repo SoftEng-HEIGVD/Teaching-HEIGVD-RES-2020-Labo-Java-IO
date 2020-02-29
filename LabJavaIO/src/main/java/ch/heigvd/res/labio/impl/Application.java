f3eff2f6aabfb47120e073897dabe0e0bb2db543
Alexandre Piveteau <alexandre.gregoire.piveteau@gmail.com>
2020-02-29 12:25:34 +0100

    Start work on Application.java and fix DFS
    The implementation of DFS traversal was working fine according to the
    provided unit tests, but it was not traversing files in the right order
    according to the specification defined by the Application class. This is
    now fixed !

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -135,11 +135,12 @@ public class Application implements IApplication {
     explorer.explore(new File(WORKSPACE_DIRECTORY), new IFileVisitor() {
       @Override
       public void visit(File file) {
-        /*
-         * There is a missing piece here. Notice how we use an anonymous class here. We provide the implementation
-         * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
-         * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
-         */
+        try {
+          writer.write(file.getPath());
+          writer.write(System.lineSeparator());
+        } catch (IOException any) {
+          throw new RuntimeException(any);
+        }
       }
     });
   }
--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -28,33 +28,17 @@ public class DFSFileExplorer implements IFileExplorer {
     // we use a stack to offer an iterative implementation and explicitly
     // manage the recursion by ourselves.
     Stack<File> deque = new Stack<>();
-
-    // We can only have directories in the stack.
     deque.push(rootDirectory);
 
     while (!deque.isEmpty()) {
-
       File current = deque.pop();
+      visitor.visit(current);
       File[] children = current.listFiles();
 
-      // Visit the current directory.
-      visitor.visit(current);
-
       if (children != null) {
-
-        // The ordering of the files read by the listFiles() method is not
-        // deterministic, so we have to force it to be done in a sorted
-        // order to pass the unit tests.
         Arrays.sort(children, Comparator.comparing(File::getName).reversed());
-
-        // Visit all the direct children, and push the
-        // subdirectories onto the stack.
         for (File child : children) {
-          if (child.isDirectory()) {
-            deque.push(child); // Defer visit to the next iteration.
-          } else {
-            visitor.visit(child);
-          }
+          deque.push(child);
         }
       }
     }
