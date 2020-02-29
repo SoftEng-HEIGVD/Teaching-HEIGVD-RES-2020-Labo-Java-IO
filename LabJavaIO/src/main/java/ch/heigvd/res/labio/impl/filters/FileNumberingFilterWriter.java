41462bc420f88d82076f3de9ea5eec5bb8c18133
Arthur <arthur.becaud@yahoo.fr>
2020-02-29 16:09:35 +0100

    FileNumbering Test passed

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.filters;
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 /**
@@ -18,24 +19,53 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int counter;
+  boolean backSlashRFlag;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    counter = 1;
+    backSlashRFlag = false;
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (off + len > str.length()) {
+      throw new IllegalArgumentException("Offset + length > than String length");
+    }
+    for (int i = off; i < off + len; ++i){
+      char c = str.charAt(i);
+      write(c);
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    write(new String(cbuf), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // write the first number at first call of function
+    if (counter == 1) {
+      out.write(counter++ + "\t");
+    }
+    // check if a \r was previously used
+    if (backSlashRFlag && c != '\n') {
+      out.write(counter++ + "\t");
+    }
+    // write char
+    out.write(c);
+    // update flag or write the number if the char is a \n
+    switch (c) {
+      case '\r':
+        backSlashRFlag = true;
+        break;
+      case '\n':
+        out.write(counter++ + "\t");
+      default:
+        backSlashRFlag = false;
+        break;
+    }
   }
-
 }
