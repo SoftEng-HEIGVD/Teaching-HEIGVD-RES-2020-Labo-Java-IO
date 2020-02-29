3320dbb30ff1086af24a06c4d3dd62f671c6ad39
Robin Müller <rm.muller@bluewin.com>
2020-02-29 23:22:14 +0100

    Fixes to pass FileNumberingFilterWriterTest

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,6 +18,8 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int line = 0;
+  private int previous;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -25,17 +27,27 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    this.write(str.toCharArray(), off, len);
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for (int i = off; i < off + len; i++) {
+      this.write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (line == 0 || (previous == '\r' && c != '\n'))
+      super.write(++line + "\t", 0, 1 + ("" + line).length());
+
+    super.write(c);
+
+    if (c == '\n')
+      super.write(++line + "\t", 0, 1 + ("" + line).length());
+
+    previous = c;
   }
 
 }
