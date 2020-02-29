e65035bbd8059fad4b1d8e40378753157a1973c8
Gaetan2907 <gaetan.daubresse@gmail.com>
2020-02-29 21:13:35 +0100

    FileNumbering completed with all the tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -19,23 +19,69 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int lineCounter = 0;
+  private boolean firstLine = true;
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    char[] cbuf = str.toCharArray();
+    this.write(cbuf, off, len);
+
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    int charCounter = 0;
+
+    if(firstLine){
+      ++lineCounter;
+      out.write(lineCounter + "\t");
+      firstLine = false;
+    }
+
+    for(int i = 0; i < len; ++i){
+      ++charCounter;
+      if((i < (len-1)) && (cbuf[off+i] == '\r') && cbuf[off+i+1] == '\n'){
+        ++lineCounter;
+        out.write(cbuf, (off+i-charCounter+1), charCounter+1);
+        out.write(lineCounter + "\t");
+        charCounter = 0;
+        ++i;
+      }
+      else if((cbuf[off+i] == '\n' || cbuf[off+i] == '\r')){
+        ++lineCounter;
+        out.write(cbuf, (off+i-charCounter+1), charCounter);
+        out.write(lineCounter + "\t");
+        charCounter = 0;
+      }
+      else if(i == (len-1)){
+        out.write(cbuf, (off+i-charCounter+1), charCounter);
+      }
+    }
+
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(firstLine){
+      ++lineCounter;
+      out.write(lineCounter + "\t");
+      firstLine = false;
+    }
+    if((char) c == '\n'){
+      ++lineCounter;
+      out.write(c);
+      out.write(lineCounter + "\t");
+    }else{
+      out.write(c);
+    }
   }
 
 }
