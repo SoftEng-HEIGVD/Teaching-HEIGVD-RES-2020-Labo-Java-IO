b2747763cd138922f04f7c33d139eb00a7067c03
KurohanaJuri <chau-ying-kot@hotmail.com>
2020-02-29 17:17:40 +0100

    All tests int UtilsTest passed

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -1,6 +1,8 @@
 package ch.heigvd.res.labio.impl;
 
+import java.lang.reflect.WildcardType;
 import java.util.logging.Logger;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -9,6 +11,15 @@ import java.util.logging.Logger;
 public class Utils {
 
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+
+  private static final String UNIX_SEPARATOR = "\n";
+  private static final String MAC_OS9_SEPARATOR = "\r";
+  private static final String WINDOWS_SEPARATOR = "\r\n";
+
+  private static final Pattern UNIX_PATTERN = Pattern.compile(UNIX_SEPARATOR);
+  private static final Pattern MAC_OS9_PATTERN = Pattern.compile(MAC_OS9_SEPARATOR);
+  private static final Pattern WINDOWS_PATTERN = Pattern.compile(WINDOWS_SEPARATOR);
+
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
@@ -20,9 +31,22 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    String[] splitedString = lines.split("\\r?\\n", 2);
-    splitedString[0] += "\n";
-    return splitedString;
+    String separator;
+    // Find which seprator is used
+    if(WINDOWS_PATTERN.matcher(lines).find()){
+      separator = WINDOWS_SEPARATOR;
+    } else if (UNIX_PATTERN.matcher(lines).find()){
+      separator = UNIX_SEPARATOR;
+    } else if(MAC_OS9_PATTERN.matcher(lines).find()){
+      separator = MAC_OS9_SEPARATOR;
+    } else { // No separator found
+      return new String[]{"", lines};
+    }
+
+    String[] splitString = lines.split(separator, 2);
+    // split method consume the separator, we need to add one manually
+    splitString[0] += separator;
+    return splitString;
   }
 
 }
