4dbe9f3b6832759e6ed1c0841def8db46813579e
Loic Dessaules <loic.dessaules@heig-vd.ch>
2020-03-01 18:03:58 +0100

    Finish DFS functionality

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -14,9 +15,26 @@ import java.io.File;
  */
 public class DFSFileExplorer implements IFileExplorer {
 
-  @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void explore(File rootDirectory, IFileVisitor visitor) {
+      visitor.visit(rootDirectory);
+      this.exploreRecursively(rootDirectory, visitor);
+    }
+
+    private void exploreRecursively(File rootDirectory, IFileVisitor visitor) {
+        // Fetch all files in the current dir
+        File[] filesInDir = rootDirectory.listFiles();
+        if(filesInDir != null){
+            // Sort the array to prevent test bug in some OS
+            Arrays.sort(filesInDir);
+            for(File file : filesInDir) {
+                visitor.visit(file);
+                // Recursive call on directory
+                if(file.isDirectory()){
+                    this.exploreRecursively(file, visitor);
+                }
+            }
+        }
+    }
 
 }
