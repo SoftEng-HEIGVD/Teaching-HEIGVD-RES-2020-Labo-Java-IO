c3598a26b919d16c16112aedef5a74741e25efd9
Naludrag <stephanecarvalho81@gmail.com>
2020-03-01 15:07:22 +0100

    Commit passes all tests for fileNumbering tests but I still think that algorithm can be improved

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -19,7 +19,9 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-  private static int nbLines = 0;
+  private int nbLines = 0;
+  private boolean lastLine = false;
+  private char lastChar = ' ';
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -27,18 +29,27 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
+      str = str.substring(off, off + len);
       String[] elements = Utils.getNextLine(str);
       while(!elements[0].equals("")) {
         //Première ligne
-        if(nbLines == 0) {
+        if(!lastLine) {
           out.write(Integer.toString(++nbLines) + '\t' + elements[0]);
         }
         else {
           out.write(elements[0]);
+          lastLine = false;
         }
         elements = Utils.getNextLine(elements[1]);
       }
-    out.write(Integer.toString(++nbLines) + '\t' + elements[0]);
+      if(lastLine){
+        out.write(elements[1]);
+        lastLine = false;
+      }
+      else {
+        out.write(Integer.toString(++nbLines) + '\t' + elements[1]);
+        lastLine = true;
+      }
   }
 
   @Override
@@ -48,7 +59,25 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    boolean newLine = false;
+    if(nbLines == 0){
+      out.write(Integer.toString(++nbLines) + '\t');
+    }
+    if((lastChar == '\n' ||  lastChar == '\r') && c == '\n') {
+      newLine = true;
+      //Changement du lastChar pour ne pas rester avec à chaque fois une fin de ligne
+      lastChar = ' ';
+    }
+    else if(lastChar == '\n' || lastChar == '\r'){
+      out.write(Integer.toString(++nbLines) + '\t');
+      lastChar = ' ';
+    }
+    if(!newLine && (c == '\n' || c == '\r'))
+      lastChar = (char)c;
+    out.write(c);
+    if(newLine){
+      out.write(Integer.toString(++nbLines) + '\t');
+    }
   }
 
 }
