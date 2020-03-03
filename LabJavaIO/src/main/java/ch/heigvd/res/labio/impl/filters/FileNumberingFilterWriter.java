7838667c20d2abc29291a9d80ca9718287200a65
Loic Dessaules <loic.dessaules@heig-vd.ch>
2020-03-03 10:13:26 +0100

    Pass some test of FileNumberFilter but still WIP

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -17,25 +17,42 @@ import java.util.logging.Logger;
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private int lineNb = 1;
+    private boolean firstLine = true;
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        if(firstLine){
+            str = lineNb++ + "\t" + str;
+            firstLine = false;
+        }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        // todo: compter combien y a de \r et replace autant de fois .. comme ca si y a 2 \r dans 1 ligne ba on fait 2 fois le lineNb++ etc...
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+        if(str.contains("\n")){
+            str = str.replaceAll("\n", "\n" + lineNb++ + "\t");
+        }else if(str.contains("\r")){
+            str = str.replaceAll("\r", "\r" + lineNb++ + "\t");
+        }else if(str.contains("\r\n")){
+            str = str.replaceAll("\r\n", "\r\n" + lineNb++ + "\t");
+        }
+
+        super.write(str, off, str.length());
+    }
+
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    }
 
 }
