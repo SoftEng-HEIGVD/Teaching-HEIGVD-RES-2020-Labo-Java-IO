7ca518aacee0ca25be18cda541eff43feb0a7f8f
Robin <r.demarta@outlook.com>
2020-03-03 22:43:46 +0100

    FileNumberingFilterWriterTest passed

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/CompleteFileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/CompleteFileTransformer.java
@@ -18,12 +18,6 @@ public class CompleteFileTransformer extends FileTransformer {
 
   @Override
   public Writer decorateWithFilters(Writer writer) {
-    /*
-     * If you uncomment the following line (and get rid of th 3 previous lines...), you will restore the decoration 
-     * of the writer (connected to the file. You can see that you first decorate the writer with an UpperCaseFilterWriter, which you then
-     * decorate with a FileNumberingFilterWriter. The resulting writer is used by the abstract class to write the characters read from the
-     * input files. So, the input is first prefixed with line numbers, then transformed to uppercase, then sent to the output file.f
-     */
     return new FileNumberingFilterWriter(new UpperCaseFilterWriter(writer));
   }
 
