91af6cdaeeec38b56d3801fe03c406c1812c33b8
Loic Dessaules <loic.dessaules@heig-vd.ch>
2020-03-03 19:01:45 +0100

    Finish some of Numbering filter implementation but still WIP for this test : itShouldHandleWriteWithAnInt

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -1,5 +1,7 @@
 package ch.heigvd.res.labio.impl.filters;
 
+import ch.heigvd.res.labio.impl.Utils;
+
 import java.io.FilterWriter;
 import java.io.IOException;
 import java.io.Writer;
@@ -18,36 +20,46 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
     private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
-    private int lineNb = 1;
-    private boolean firstLine = true;
+    private int lineNb = 0;
 
     public FileNumberingFilterWriter(Writer out) {
         super(out);
     }
 
+    private void writeLineNb() throws IOException {
+        out.write((++lineNb) + "\t");
+    }
+
     @Override
     public void write(String str, int off, int len) throws IOException {
-        if(firstLine){
-            str = lineNb++ + "\t" + str;
-            firstLine = false;
+        String[] lines = Utils.getNextLine(str.substring(off, off+len));
+
+        // First time only display the line nb
+        if(lineNb == 0){
+            writeLineNb();
         }
 
-        // todo: compter combien y a de \r et replace autant de fois .. comme ca si y a 2 \r dans 1 ligne ba on fait 2 fois le lineNb++ etc...
-
-        if(str.contains("\n")){
-            str = str.replaceAll("\n", "\n" + lineNb++ + "\t");
-        }else if(str.contains("\r")){
-            str = str.replaceAll("\r", "\r" + lineNb++ + "\t");
-        }else if(str.contains("\r\n")){
-            str = str.replaceAll("\r\n", "\r\n" + lineNb++ + "\t");
+        // There is some line return
+        if(!lines[0].equals("")) {
+            // Write the line and the line nb
+            out.write(lines[0]);
+            writeLineNb();
+            // Write next line
+            if(!lines[1].equals("")){
+                write(lines[1]);
+            }
+        }
+        // No return line so only one simple line remaining
+        else if(!lines[1].equals("")){
+            out.write(lines[1]);
         }
 
-        super.write(str, off, str.length());
+
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        write(String.valueOf(cbuf), off, len);
     }
 
     @Override
