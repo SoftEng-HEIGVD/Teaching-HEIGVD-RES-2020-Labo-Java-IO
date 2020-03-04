3fde38541ab10e2f12f6c24096b15e35e469809c
Doran Kayoumi <dorankayoumi@gmail.com>
2020-03-04 13:52:53 +0100

    fix both DFSFileExplorer  unit tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +17,32 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    vistor.visit(rootDirectory);
+
+    // explore the content of the given root directory
+    this._explore(rootDirectory, vistor);
+  }
+
+  private void _explore(File root, IFileVisitor vistor) {
+    File[] content = root.listFiles();
+
+    // if the given root doesn't have any children
+    // we don't need to continue
+    if (content == null)
+      return;
+
+    // sort the File array as it may cause some issues
+    Arrays.sort(content);
+
+    for (File file : content) {
+      vistor.visit(file);
+
+      // if the current file is a directory
+      // explore the children
+      if (file.isDirectory())
+        this._explore(file, vistor);
+    }
   }
 
 }
