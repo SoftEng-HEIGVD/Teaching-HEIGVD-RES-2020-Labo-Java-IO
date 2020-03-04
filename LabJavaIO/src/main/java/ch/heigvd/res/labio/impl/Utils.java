222678857e17ac34dd98affd5f4a81d6e69b8348
Robin Müller <rm.muller@bluewin.ch>
2020-03-04 10:45:12 +0100

    UtilsTest passed

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -1,6 +1,8 @@
 package ch.heigvd.res.labio.impl;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -9,6 +11,9 @@ import java.util.logging.Logger;
 public class Utils {
 
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+
+  //Didn't find a way to use a positive lookbehind with variable-length \R (\R matches CR, LF and CRLF)
+  private static final String LINE_REGEX = "(?<=(\r\n))|(?<=\n)|((?<=\r)(?!\n))";
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
@@ -20,7 +25,13 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //Get first line with newline and the rest
+    String[] content = lines.split(LINE_REGEX, 2);
+
+    if (content.length == 2)
+      return content;
+    else
+      return new String[]{"", content[0]};
   }
 
 }
