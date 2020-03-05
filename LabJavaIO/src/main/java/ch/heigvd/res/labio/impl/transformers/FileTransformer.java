f38a3ddd020955be5a65df83485c658520ef4dd4
Doran Kayoumi <dorankayoumi@gmail.com>
2020-03-05 08:51:23 +0100

    fix all transformers unit tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/FileTransformer.java
@@ -58,7 +58,12 @@ public abstract class FileTransformer implements IFileVisitor {
        * writer has been decorated by the concrete subclass!). You need to write a loop to read the
        * characters and write them to the writer.
        */
-      
+      int c = reader.read();
+      while (c != -1) {
+        writer.write(c);
+        c = reader.read();
+      }
+
       reader.close();
       writer.flush();
       writer.close();
