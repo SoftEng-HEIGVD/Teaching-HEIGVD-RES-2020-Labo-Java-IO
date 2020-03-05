019cf33d33677461d8704669c90eeb883528d07d
vitorva <vitor.vazafonso@gmail.com>
2020-03-05 18:20:01 +0100

    Implementation of Utils.java

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -1,6 +1,8 @@
 package ch.heigvd.res.labio.impl;
 
 import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  *
@@ -12,15 +14,26 @@ public class Utils {
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
+   * the next line in the string passed in arguments.
+   *
    * @param lines a string that may contain 0, 1 or more lines
    * @return an array with 2 elements; the first element is the next line with
    * the line separator, the second element is the remaining text. If the argument does not
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    String regex = "(?<=\r\n)|(?<=\r)(?!\n)|(?<=\n)";
+
+    String[] stringLines = lines.split(regex, 2);
+
+    // There are one or more line separators
+    if(stringLines.length == 2){
+      return stringLines;
+    }else{ // There are no line separators
+      return new String[]{"",stringLines[0]};
+    }
+
   }
 
 }
