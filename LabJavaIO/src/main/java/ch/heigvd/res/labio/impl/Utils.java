4101b6b207c21b015b57ec82d035bd0c723f7815
Laurent Scherer <lscherer@protonmail.com>
2020-03-06 19:39:49 +0100

    trying out some implementation for utils

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -3,24 +3,33 @@ package ch.heigvd.res.labio.impl;
 import java.util.logging.Logger;
 
 /**
- *
  * @author Olivier Liechti
  */
-public class Utils {
+public class Utils
+{
 
-  private static final Logger LOG = Logger.getLogger(Utils.class.getName());
+	private static final char   CR  = '\r'; // carriage return
+	private static final char   LF  = '\n'; // linefeed
+	private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
-  /**
-   * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
-   * 
-   * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
-   */
-  public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+	/**
+	 * This method looks for the next new line separators (\r, \n, \r\n) to extract
+	 * the next line in the string passed in arguments.
+	 *
+	 * @param lines a string that may contain 0, 1 or more lines
+	 *
+	 * @return an array with 2 elements; the first element is the next line with
+	 * the line separator, the second element is the remaining text. If the argument does
+	 * not
+	 * contain any line separator, then the first element is an empty string.
+	 */
+	public static String[] getNextLine(String lines)
+	{
+		int posCR = lines.indexOf(CR);
+		int posLF = lines.indexOf(LF);
+		int pos   = posCR + 1;
+
+		return new String[]{"", ""};
+	}
 
 }
