efd3a47262c56b130767c3978dea48d33c2bf278
Brassens <pablo.mercado@heig-vd.ch>
2020-03-06 15:52:01 +0100

    will work on home computer, laptop sux

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Objects;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,8 +17,14 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(!Objects.isNull(rootDirectory.listFiles())){
+      for(File file: rootDirectory.listFiles()){
+        if(file.isDirectory()){
+          explore(file, vistor);
+        }
+        vistor.visit(file);
+      }
+    }
   }
 
 }
