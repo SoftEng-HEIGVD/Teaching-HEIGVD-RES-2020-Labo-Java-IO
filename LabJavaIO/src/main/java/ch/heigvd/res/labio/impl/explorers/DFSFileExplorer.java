8e3a6ac3ae44937ce21336b76ca98b43b3dcb810
Serphentas <Serphentas@users.noreply.github.com>
2020-03-06 18:16:23 +0100

    fix DFS

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -10,28 +11,47 @@ import java.io.File;
  * node (file and directory). When the explorer reaches a directory, it visits
  * all files in the directory and then moves into the subdirectories.
  *
- * @author Olivier Liechti
+ * @author Olivier Liechti & Moïn DANAI
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   /**
-   * Explores the given {@code rootDirectory} in DFS, browsing its child folders
+   * Explores the given {@code rootDirectory} in DFS, browsing its child directories
    * first and then its child files, in ascending alphabetical order.
    * <p>
    * Each leaf file is eventually visited by the {@code visitor}.
    *
    * @author Moïn DANAI
+   *
+   * @param rootDirectory root node
+   * @param visitor method to call upon node visit
    */
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    File[] child_dirs = rootDirectory.listFiles((File node) -> node.isDirectory()),
-        child_files = rootDirectory.listFiles((File node) -> !node.isDirectory());
-
-    for (File dir : child_dirs) {
-      explore(dir, vistor);
+    if (rootDirectory == null) {
+      return;
     }
-    for (File file : child_files) {
-      vistor.visit(file);
+
+    // get children nodes
+    File[] childrenDirs = rootDirectory.listFiles((File node) -> node.isDirectory()),
+        childrenFiles = rootDirectory.listFiles((File node) -> !node.isDirectory());
+
+    vistor.visit(rootDirectory);
+
+    // visit directories
+    if (childrenDirs != null) {
+      Arrays.sort(childrenDirs);
+      for (File dir : childrenDirs) {
+        explore(dir, vistor);
+      }
+    }
+
+    // visit files
+    if (childrenFiles != null) {
+      Arrays.sort(childrenFiles);
+      for (File file : childrenFiles) {
+        vistor.visit(file);
+      }
     }
   }
 }
