5a1c8ee330f9ab7843d898d0322899610d1e7853
scout407 <stephane.bouyiatiotis@heig-vd.ch>
2020-03-06 15:25:23 +0100

    Fin du FileNumbering

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -10,32 +10,50 @@ import java.util.logging.Logger;
  * When filter encounters a line separator, it sends it to the decorated writer.
  * It then sends the line number and a tab character, before resuming the write
  * process.
- *
+ * <p>
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
-  private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+    private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
-  public FileNumberingFilterWriter(Writer out) {
-    super(out);
-  }
+    //utilisé pour le write(int c)
+    private int nbrLine;
+    private boolean isBackR;
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    public FileNumberingFilterWriter(Writer out) {
+        super(out);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        write(str.toCharArray(), off, len);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = 0; i < len; ++i) {
+            write(cbuf[i + off]);
+        }
+    }
+
+    @Override
+    public void write(int c) throws IOException {
+        if (nbrLine == 0 || c != '\n' && isBackR) {
+            nbrLine++;
+            out.write(nbrLine + "\t");
+        }
+
+        out.write(c);
+
+        if (c == '\n') {
+            out.write(++nbrLine + "\t");
+        }
+
+        isBackR = c == '\r';
+    }
 
 }
+
