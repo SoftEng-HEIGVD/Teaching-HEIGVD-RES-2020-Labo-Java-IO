3a6030fbd787451ce573c2ddb315e8d01b0d9825
Moromir <moromir24@gmail.com>
2020-03-06 17:30:49 +0100

    Passed UtilsTest.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -12,15 +12,27 @@ public class Utils {
 
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
-   * the next line in the string passed in arguments. 
+   * the next line in the string passed in arguments.
    * 
    * @param lines a string that may contain 0, 1 or more lines
-   * @return an array with 2 elements; the first element is the next line with
-   * the line separator, the second element is the remaining text. If the argument does not
-   * contain any line separator, then the first element is an empty string.
+   * @return an array with 2 elements; the first element is the next line with the
+   *         line separator, the second element is the remaining text. If the
+   *         argument does not contain any line separator, then the first element
+   *         is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (lines.contains("\r\n")) {
+
+      return new String[] { lines.substring(0, lines.indexOf("\r\n") + 2), lines.substring(lines.indexOf("\r\n") + 2) };
+    } else if (lines.contains("\r")) {
+      return new String[] { lines.substring(0, lines.indexOf("\r") + 1), lines.substring(lines.indexOf("\r") + 1) };
+
+    } else if (lines.contains("\n")) {
+      return new String[] { lines.substring(0, lines.indexOf("\n") + 1), lines.substring(lines.indexOf("\n") + 1) };
+
+    } else {
+      return new String[] { "", lines };
+    }
   }
 
 }
