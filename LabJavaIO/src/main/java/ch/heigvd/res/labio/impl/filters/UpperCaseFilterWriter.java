262b38f2a9a7cc2d34da0c49ab277564e56b5b26
Stephane Bottin <stephane.bottin@heig-vd.ch>
2020-03-06 14:44:48 +0100

    Refactor UpperCaseFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -5,30 +5,27 @@ import java.io.IOException;
 import java.io.Writer;
 
 /**
- *
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
-  public UpperCaseFilterWriter(Writer wrappedWriter) {
-    super(wrappedWriter);
-  }
 
-  @Override
-  public void write(String str, int off, int len) throws IOException {
-    super.write(str.toUpperCase(), off, len);
-  }
+    public UpperCaseFilterWriter(Writer wrappedWriter) {
+        super(wrappedWriter);
+    }
 
-  @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    for(int i = off; i < off + len; ++i)
-      cbuf[i] = Character.toUpperCase(cbuf[i]);
+    @Override
+    public void write(String str, int off, int len) throws IOException {
+        this.write(str.toCharArray(), off, len);
+    }
 
-    super.write(cbuf, off, len);
-  }
+    @Override
+    public void write(char[] cbuf, int off, int len) throws IOException {
+        for (int i = off; i < off + len; ++i)
+            this.write(cbuf[i]);
+    }
 
-  @Override
-  public void write(int c) throws IOException {
-    super.write(Character.toUpperCase(c));
-  }
+    @Override
+    public void write(int c) throws IOException {
+        super.write(Character.toUpperCase(c));
+    }
 }
