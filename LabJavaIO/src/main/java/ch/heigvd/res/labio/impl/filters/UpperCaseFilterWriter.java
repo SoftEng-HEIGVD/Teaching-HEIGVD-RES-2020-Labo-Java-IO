630223340a59cd2d573c6a1debbd8c48c807216d
Serphentas <Serphentas@users.noreply.github.com>
2020-03-06 18:16:43 +0100

    refactor UpperCaseFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -37,7 +37,7 @@ public class UpperCaseFilterWriter extends FilterWriter {
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
     for (int i = off; i < len + off; ++i) {
-      write(Character.toUpperCase(cbuf[i]));
+      write(cbuf[i]);
     }
   }
 
