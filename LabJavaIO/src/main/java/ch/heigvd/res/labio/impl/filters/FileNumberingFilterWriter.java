a62e2c5b695122043f6a42f3cee7bd746addce47
NR09 <nenad_rajic@hotmail.com>
2020-03-06 14:07:42 +0100

    FileNumberingFilterWriter test pass[ok]

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,6 +18,11 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int nbLines = 1; //Le numéro de ligne commence à 1
+  private int prevChar = 0;
+  private static final char NEW_LINE = '\n';
+  private static final char TAB = '\t';
+  private static final char RETURN = '\r';
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -36,10 +41,30 @@ public class FileNumberingFilterWriter extends FilterWriter {
       write(cbuf[i]);
     }
   }
-
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+    final String STR_LINE_NB = String.valueOf(nbLines);
 
+    //Ici, si c'est la première ligne OU si le caractère ne correpsond pas au séparateur de ligne '\n'
+    // Dans le cas de MAC, il est également nécessaire de vérifier si le caractère précédent est un return '\r'
+    if(nbLines == 1 || prevChar == RETURN && c != NEW_LINE){
+      // Ecritur du numéro de ligne au début du text traité
+      super.write(STR_LINE_NB,0,String.valueOf(nbLines).length());
+      // Ajout d'une tabulation après chaque numéro de ligne et incrémentation
+      super.write(TAB);
+      ++nbLines;
+    }
+    // Ecriture du caractère traité à la suite
+    super.write(c);
+
+    // Verification nouvelle ligne sous Win/Unix
+    if(c == NEW_LINE){
+      // Ecriture du numéro de ligne, d'un tabulation et incrémentation du nb de lignes
+      super.write(STR_LINE_NB, 0, String.valueOf(nbLines).length());
+      super.write(TAB);
+      ++nbLines;
+    }
+    // enregistrement du car traité précédement pour le prochain appel de la fonction
+    prevChar = c;
+  }
 }
