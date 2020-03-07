10be111913ad9fdadd5f2617ac30862224668472
Brassens <pablo.mercado@heig-vd.ch>
2020-03-07 16:31:24 +0100

    DFSFileExplorerTest ok

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 import java.util.Objects;
 
 /**
@@ -10,20 +11,22 @@ import java.util.Objects;
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    if(!Objects.isNull(rootDirectory.listFiles())){
-      vistor.visit(rootDirectory);
+    if(Objects.isNull(rootDirectory)){ return; }
 
-      for(File file: rootDirectory.listFiles()){
-        if(file.isDirectory()){
-          explore(file, vistor);
-        }
+    vistor.visit(rootDirectory);
+
+    File[] files = rootDirectory.listFiles();
+
+    if(!Objects.isNull(files) && files.length > 0) {
+      for(File file: files){
+        explore(file, vistor);
       }
     }
   }
