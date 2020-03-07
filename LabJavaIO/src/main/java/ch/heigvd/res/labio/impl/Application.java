b45dede5698b7ef18581400b4bfff5f1f1f304db
Rosy-Laure Wonjamouna <laure.wonjamouna@gmail.com>
2020-03-07 22:10:08 +0100

    tests au vert + modification numerotation des fichiers

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -89,7 +89,7 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
-      storeQuote(quote, new File("quote-"+i+".utf8"));
+      storeQuote(quote, new File("quote-"+i+1+".utf8"));
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
