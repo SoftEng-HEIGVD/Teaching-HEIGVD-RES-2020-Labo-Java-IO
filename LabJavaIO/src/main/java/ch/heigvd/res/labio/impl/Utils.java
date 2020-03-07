f60554127cd95ae4f018d5d1c832d43f0261ee4c
Rosy-Laure Wonjamouna <laure.wonjamouna@gmail.com>
2020-03-07 20:59:06 +0100

    Utils Tests passed! But I never use Utils function...

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -10,6 +10,7 @@ public class Utils {
 
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
+
   /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
    * the next line in the string passed in arguments. 
@@ -20,7 +21,27 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String[] str = new String[2];
+    int len = lines.length();
+
+    for(int i = 0; i < len; i++){
+      if(i < len -1) {
+        if ((lines.charAt(i) == '\r') && (lines.charAt(i + 1) == '\n')) {
+          str[0] = lines.substring(0, i + 2);
+          str[1] = lines.substring(i + 2, len);
+          return str;
+        }
+      }
+     if((lines.charAt(i) == '\r')  || (lines.charAt(i) == '\n')){
+        str[0] = lines.substring(0, i+1);
+        str[1] = lines.substring(i+1,len);
+        return str;
+      }
+   }
+    str[0] = "";
+    str[1] = lines;
+    return str;
+
   }
 
 }
