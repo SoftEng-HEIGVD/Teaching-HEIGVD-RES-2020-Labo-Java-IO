5273beed6498550c7beb525d0796b2dc43439857
alfavre <fav.alban@gmail.com>
2020-03-07 19:34:54 +0100

    all transformer test passed

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/CompleteFileTransformer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/transformers/CompleteFileTransformer.java
@@ -1,5 +1,8 @@
 package ch.heigvd.res.labio.impl.transformers;
 
+import ch.heigvd.res.labio.impl.filters.FileNumberingFilterWriter;
+import ch.heigvd.res.labio.impl.filters.UpperCaseFilterWriter;
+
 import java.io.Writer;
 
 /**
@@ -8,24 +11,15 @@ import java.io.Writer;
  * When an instance of this class is passed to a file system explorer, it will
  * generate an output file with 1) uppercase letters and 2) line numbers at the
  * beginning of each line.
- * 
+ *
  * @author Olivier Liechti
  */
 public class CompleteFileTransformer extends FileTransformer {
 
-  @Override
-  public Writer decorateWithFilters(Writer writer) {
-    if (true) {
-      throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    @Override
+    public Writer decorateWithFilters(Writer writer) {
+        writer = new FileNumberingFilterWriter(new UpperCaseFilterWriter(writer));
+        return writer;
     }
-    /*
-     * If you uncomment the following line (and get rid of th 3 previous lines...), you will restore the decoration 
-     * of the writer (connected to the file. You can see that you first decorate the writer with an UpperCaseFilterWriter, which you then
-     * decorate with a FileNumberingFilterWriter. The resulting writer is used by the abstract class to write the characters read from the
-     * input files. So, the input is first prefixed with line numbers, then transformed to uppercase, then sent to the output file.f
-     */
-    //writer = new FileNumberingFilterWriter(new UpperCaseFilterWriter(writer));
-    return writer; 
-  }
 
 }
