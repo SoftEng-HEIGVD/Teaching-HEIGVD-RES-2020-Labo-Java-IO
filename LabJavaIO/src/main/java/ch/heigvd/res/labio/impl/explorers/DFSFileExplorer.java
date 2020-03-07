47fdbe97a8b6ded6bb0167d4bd4682e7ad275913
Nic0Mueller <nimu80@gmail.com>
2020-03-07 13:24:25 +0100

    DFS File explorer passing all tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -10,13 +11,36 @@ import java.io.File;
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
  * 
- * @author Olivier Liechti
+ * @author Olivier Liechti - Modified by Nicolas Müller on 07.03.2020
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+  public void explore(File rootDirectory, IFileVisitor visitor) {
 
+    visitor.visit(rootDirectory);
+
+    // No need to do anything more if it's not a directory
+    if (!rootDirectory.isDirectory()) {
+
+      return;
+    }
+
+    File[] files = rootDirectory.listFiles();
+
+    // No need to do anything more if directory is empty
+    if (files != null) {
+
+      // Test sometimes fails without sorting
+      Arrays.sort(files);
+
+      for (File f : files) {
+
+        if (f.isDirectory()) {
+
+          explore(f, visitor);
+        }
+      }
+    }
+  }
 }
