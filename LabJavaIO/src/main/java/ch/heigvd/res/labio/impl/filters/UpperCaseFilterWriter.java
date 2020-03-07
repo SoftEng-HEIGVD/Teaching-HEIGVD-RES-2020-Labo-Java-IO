72b4c067c924d68913cabdce2a22b9e4ffbe24c4
alfavre <fav.alban@gmail.com>
2020-03-07 15:30:01 +0100

    UpperCaseFilter passed all tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -15,17 +15,28 @@ public class UpperCaseFilterWriter extends FilterWriter {
 
     @Override
     public void write(String str, int off, int len) throws IOException {
-
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < str.length(); i++) {
+            sb.append(Character.toUpperCase(str.charAt(i)));
+        }
+        //we let super handle the off and len
+        super.write(sb.toString(), off, len);
     }
 
     @Override
     public void write(char[] cbuf, int off, int len) throws IOException {
-        throw new UnsupportedOperationException("The student has not implemented this method yet.");
+        char[] cString = new char[cbuf.length];
+        for (int i = 0; i < cbuf.length; i++) {
+            cString[i] = Character.toUpperCase(cbuf[i]);
+        }
+        //we let super handle the off and len
+        super.write(cString, off, len);
     }
 
     @Override
     public void write(int c) throws IOException {
         super.write(Character.toUpperCase(c));
     }
+
 
 }
