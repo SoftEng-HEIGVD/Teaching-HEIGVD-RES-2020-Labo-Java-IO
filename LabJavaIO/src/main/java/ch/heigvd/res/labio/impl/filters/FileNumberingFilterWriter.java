3bbb36c0a82a8484f76511e75594b1ad86354500
doeni98 <denis_bourqui@hotmail.com>
2020-03-08 12:05:10 +0100

    passing fileNumbering & all tests ok

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -11,13 +11,20 @@ import java.util.logging.Logger;
  * It then sends the line number and a tab character, before resuming the write
  * process.
  *
- * Hello\n\World -> 1\Hello\n2\tWorld
+ * Hello\n\World -> 1\tHello\n2\tWorld
  *
  * @author Olivier Liechti
+ *
+ * \n -> \n[number]\t
+ * \r -> \r[number]\t
+ * \r\n -> \r\n[number]\t
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int linesCounter = 1;
+  private boolean newLine = true;
+  private int charBefore = 0;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
@@ -25,17 +32,37 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < len + off; i++){
+      write(str.charAt(i));
+    }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    for(int i = off; i < len + off; i++){
+      write(cbuf[i]);
+    }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(newLine){ //used for first line only
+      writeLinePrefix();
+      newLine = false;
+    }
+    if(c != '\n' && charBefore == '\r'){
+      writeLinePrefix();
+    }
+    super.write(c);
+    
+    if(c == '\n'){
+      writeLinePrefix();
+    }
+    charBefore = c;
+  }
+  public void writeLinePrefix() throws IOException {
+    super.write(linesCounter + "\t", 0, (int) (Math.log10(linesCounter) + 2));
+    linesCounter++;
   }
 
 }
