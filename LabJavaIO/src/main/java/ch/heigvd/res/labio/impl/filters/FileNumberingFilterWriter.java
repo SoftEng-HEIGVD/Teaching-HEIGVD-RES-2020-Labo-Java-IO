02a91b8a3b94dbea502138dc6980fb92bda51496
Gildas <gildas.houlmann@gmail.com>
2020-03-08 21:33:12 +0100

    fix Application

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -18,24 +18,65 @@ import java.util.logging.Logger;
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int lineNumber;
+  private boolean lastCharWasAReturn;
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
+    this.lineNumber = 1;
+    this.lastCharWasAReturn = false;
+  }
+
+  private int isSeparator(char c, char c2){
+    if(c == '\r'){
+      if(c2 == '\n')
+        return 2;
+      else {
+        lastCharWasAReturn = true;
+        return 1;
+      }
+    }
+    if(c == '\n') return 1;
+    return 0;
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    StringBuilder newStr = new StringBuilder();
+    if(lineNumber == 1) newStr.append(lineNumber++ + "\t");
+    boolean EOF = false;
+    for(int i = off; i < len + off; ++i){
+      if(i+1 >= len + off) EOF = true;
+      char c = str.charAt(i);
+      newStr.append(c);
+        int isSeparator = (isSeparator(c, EOF? 0 : str.charAt(i+1)));
+        if(isSeparator != 0){
+          if(isSeparator == 2){
+            ++i;
+            newStr.append(str.charAt(i));
+          }
+          newStr.append(lineNumber++);
+          newStr.append("\t");
+        }
+    }
+    super.write(newStr.toString(), 0, newStr.length());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String s = new String(cbuf);
+    this.write(s, off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    //throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if(lastCharWasAReturn && c == '\n'){
+      super.write("\n", 0, 1);
+      lastCharWasAReturn = false;
+    }
+    else
+      this.write(Character.toString((char)c), 0, 1);
   }
 
 }
