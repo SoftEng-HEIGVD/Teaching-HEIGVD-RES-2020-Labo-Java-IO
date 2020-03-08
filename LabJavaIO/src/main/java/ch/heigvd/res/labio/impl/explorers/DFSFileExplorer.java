0dfdf4f544518e47f506f353a0d2afc23c81dd5a
Lindwing <rui.lopesgouveia@heig-vd.ch>
2020-03-08 21:03:28 +0100

    complete DFS

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +17,16 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    vistor.visit(rootDirectory);
+
+    if (rootDirectory.isDirectory()) {
+      File[] listFile = rootDirectory.listFiles();
+      Arrays.sort(listFile);
+
+      for (int i = 0; i < listFile.length; ++i) {
+        explore(listFile[i], vistor);
+      }
+    }
   }
 
 }
