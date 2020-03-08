539eddf8cc37d72f2915f36940e216409aae0e8e
Moromir <moromir24@gmail.com>
2020-03-09 00:30:14 +0100

    Fixed last Application test.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -30,19 +30,18 @@ public class Application implements IApplication {
    * to where the Java application is invoked.
    */
   public static String WORKSPACE_DIRECTORY = "./workspace/quotes";
-  
+
   private static final Logger LOG = Logger.getLogger(Application.class.getName());
-  
+
   public static void main(String[] args) {
-    
+
     /*
      * I prefer to have LOG output on a single line, it's easier to read. Being able
      * to change the formatting of console outputs is one of the reasons why it is
      * better to use a Logger rather than using System.out.println
      */
     System.setProperty("java.util.logging.SimpleFormatter.format", "%4$s: %5$s%6$s%n");
-    
-       
+
     int numberOfQuotes = 0;
     try {
       numberOfQuotes = Integer.parseInt(args[0]);
@@ -50,32 +49,33 @@ public class Application implements IApplication {
       System.err.println("The command accepts a single numeric argument (number of quotes to fetch)");
       System.exit(-1);
     }
-        
+
     Application app = new Application();
     try {
       /*
        * Step 1 : clear the output directory
        */
       app.clearOutputDirectory();
-      
+
       /*
        * Step 2 : use the QuotesClient to fetch quotes; store each quote in a file
        */
       app.fetchAndStoreQuotes(numberOfQuotes);
-      
+
       /*
-       * Step 3 : use a file explorer to traverse the file system; print the name of each directory and file
+       * Step 3 : use a file explorer to traverse the file system; print the name of
+       * each directory and file
        */
       Writer writer = new StringWriter(); // we create a special writer that will send characters into a string (memory)
-      app.printFileNames(writer);         // we hand over this writer to the printFileNames method
-      LOG.info(writer.toString());       // we dump the whole result on the console
-      
+      app.printFileNames(writer); // we hand over this writer to the printFileNames method
+      LOG.info(writer.toString()); // we dump the whole result on the console
+
       /*
-       * Step 4 : process the quote files, by applying 2 transformations to their content
-       *          (convert to uppercase and add line numbers)
+       * Step 4 : process the quote files, by applying 2 transformations to their
+       * content (convert to uppercase and add line numbers)
        */
       app.processQuoteFiles();
-      
+
     } catch (IOException ex) {
       LOG.log(Level.SEVERE, "Could not fetch quotes. {0}", ex.getMessage());
       ex.printStackTrace();
@@ -104,31 +104,32 @@ public class Application implements IApplication {
       }
     }
   }
-  
+
   /**
    * This method deletes the WORKSPACE_DIRECTORY and its content. It uses the
    * apache commons-io library. You should call this method in the main method.
    * 
-   * @throws IOException 
+   * @throws IOException
    */
   void clearOutputDirectory() throws IOException {
-    FileUtils.deleteDirectory(new File(WORKSPACE_DIRECTORY));    
+    FileUtils.deleteDirectory(new File(WORKSPACE_DIRECTORY));
   }
 
   /**
-   * This method stores the content of a quote in the local file system. It has
-   * 2 responsibilities: 
+   * This method stores the content of a quote in the local file system. It has 2
+   * responsibilities:
    * 
-   * - with quote.getTags(), it gets a list of tags and uses
-   *   it to create sub-folders (for instance, if a quote has three tags "A", "B" and
-   *   "C", it will be stored in /quotes/A/B/C/quotes-n.utf8.
+   * - with quote.getTags(), it gets a list of tags and uses it to create
+   * sub-folders (for instance, if a quote has three tags "A", "B" and "C", it
+   * will be stored in /quotes/A/B/C/quotes-n.utf8.
    * 
    * - with quote.getQuote(), it has access to the text of the quote. It stores
-   *   this text in UTF-8 file.
+   * this text in UTF-8 file.
    * 
-   * @param quote the quote object, with tags and text
-   * @param filename the name of the file to create and where to store the quote text
-   * @throws IOException 
+   * @param quote    the quote object, with tags and text
+   * @param filename the name of the file to create and where to store the quote
+   *                 text
+   * @throws IOException
    */
   void storeQuote(Quote quote, String filename) throws IOException {
 
@@ -149,10 +150,10 @@ public class Application implements IApplication {
     oswriter.flush();
     oswriter.close();
   }
-  
+
   /**
-   * This method uses a IFileExplorer to explore the file system and prints the name of each
-   * encountered file and directory.
+   * This method uses a IFileExplorer to explore the file system and prints the
+   * name of each encountered file and directory.
    */
   void printFileNames(final Writer writer) {
     IFileExplorer explorer = new DFSFileExplorer();
@@ -172,7 +173,10 @@ public class Application implements IApplication {
 
         try {
           writer.write(file.getPath());
-          writer.write(System.lineSeparator());
+          // writer.write(System.lineSeparator()); // Deleted to pass the following test :
+          writer.write("\n"); // Fixed this to pass the theApplicationShouldBeAbleToGenerateTheListOfFileNames
+                              // Test. On windows system it writes \r\n and the tests wants only \n.
+
         } catch (IOException e) {
           throw new RuntimeException(e);
         }
@@ -183,7 +187,7 @@ public class Application implements IApplication {
   @Override
   public void processQuoteFiles() throws IOException {
     IFileExplorer explorer = new DFSFileExplorer();
-    explorer.explore(new File(WORKSPACE_DIRECTORY), new CompleteFileTransformer());    
+    explorer.explore(new File(WORKSPACE_DIRECTORY), new CompleteFileTransformer());
   }
 
 }
