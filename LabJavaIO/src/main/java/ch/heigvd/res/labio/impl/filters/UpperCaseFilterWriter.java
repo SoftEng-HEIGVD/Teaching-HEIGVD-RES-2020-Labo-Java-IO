a859a9c7608d34b977e9ceacdeab6239b8aa46b3
GTZL1 <duprazdavid@gmail.com>
2020-03-08 20:09:16 +0100

    upper transformation tests work

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/UpperCaseFilterWriter.java
@@ -9,24 +9,46 @@ import java.io.Writer;
  * @author Olivier Liechti
  */
 public class UpperCaseFilterWriter extends FilterWriter {
-  
+
+  private final int LOWER_MIN =97;
+  private final int LOWER_MAX = 122;
+  private final int LOW_UP_GAP=32;
+
   public UpperCaseFilterWriter(Writer wrappedWriter) {
     super(wrappedWriter);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      for(int x=off;x<(off+len);++x)
+      {
+          out.write(convertChar(str.charAt(x)));
+      }
   }
 
   @Override
-  public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+  public void write(char[] cbuf, int off, int len) throws IOException
+  {
+      for(int x=off;x<(off+len);++x)
+      {
+        out.write(convertChar(cbuf[x]));
+      }
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      out.write(convertChar((char)c));
   }
 
+  private char convertChar(char c)
+  {
+      if(c>=LOWER_MIN && c<=LOWER_MAX)
+      {
+          return ((char)(c-=LOW_UP_GAP));
+      }
+      else
+      {
+          return c;
+      }
+  }
 }
