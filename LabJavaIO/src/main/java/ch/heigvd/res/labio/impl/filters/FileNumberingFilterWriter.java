2d761b9b99ff060864bd6cd32d9cf44a88941fae
Brunisholz Julien <julien.brunisholz@gmail.com>
2020-03-08 20:20:26 +0100

    Added FileNumbering

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -19,23 +19,53 @@ public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
 
+  private int lineNumber = 1;
+  private int lastCharacter = '\0';
+
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      for (int i = off; i < off + len; i++) {
+          write(str.charAt(i));
+      }
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+      write(String.valueOf(cbuf), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    switch (c){
+        case '\n' :
+            if(lastCharacter != '\r'){
+                out.write("\n");
+            }
+            else{
+                out.write("\r\n");
+            }
+            out.write(String.valueOf(lineNumber++));
+            out.write("\t");
+            lastCharacter = '\n';
+            break;
+        case '\r' :
+            lastCharacter = '\r';
+            break;
+        default :
+            if(lastCharacter == '\0'){
+                out.write(String.valueOf(lineNumber++) + '\t');
+            }
+            if(lastCharacter == '\r'){
+                out.write('\r');
+                out.write(String.valueOf(lineNumber++) + '\t');
+            }
+            out.write(c);
+            lastCharacter = c;
+    }
   }
 
 }
