a7f6d74c6313cca8d91387c6711d228fc9af67dc
Julien Béguin <julien.beguin99@gmail.com>
2020-03-08 04:35:21 +0100

    Fix FileNumberingFilterWriter

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/filters/FileNumberingFilterWriter.java
@@ -14,28 +14,55 @@ import java.util.logging.Logger;
  * Hello\n\World -> 1\Hello\n2\tWorld
  *
  * @author Olivier Liechti
+ * Modified by Julien Béguin on 08.03.2020
  */
 public class FileNumberingFilterWriter extends FilterWriter {
 
   private static final Logger LOG = Logger.getLogger(FileNumberingFilterWriter.class.getName());
+  private int index = 0;
+  private char lastChar = '\0';
 
   public FileNumberingFilterWriter(Writer out) {
     super(out);
   }
 
+  private String custom(int c) {
+    StringBuilder sb = new StringBuilder();
+
+    if (index == 0 || (lastChar == '\r' && c != '\n')) {
+      sb.append(++index).append('\t');
+    }
+
+    sb.append((char) c);
+    lastChar = (char) c;
+
+    if (c == '\n') {
+      sb.append(++index).append('\t');
+    }
+
+    return sb.toString();
+  }
+
   @Override
   public void write(String str, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    if (off < 0 || len < 1 || off + len > str.length()) {
+      throw new IOException("Invalid arguments. Offset or length not in range.");
+    }
+    StringBuilder sb = new StringBuilder();
+    for (int i = off; i < off + len; i++) {
+      sb.append(custom(str.charAt(i)));
+    }
+    out.write(sb.toString());
   }
 
   @Override
   public void write(char[] cbuf, int off, int len) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    write(new String(cbuf), off, len);
   }
 
   @Override
   public void write(int c) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    out.write(custom(c));
   }
 
 }
