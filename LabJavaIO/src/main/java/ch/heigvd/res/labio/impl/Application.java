6a33801b87442e151132d3eb375c01443fc006dc
alfavre <fav.alban@gmail.com>
2020-03-08 13:57:40 +0100

    Application passed all tests

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,13 +7,13 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
+
 
 /**
  *
@@ -90,6 +90,9 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+
+      storeQuote(quote,String.format("quote-%d", i));
+
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +126,29 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+
+    //we create the file name
+    char backslash = 92;
+    StringBuilder sb = new StringBuilder();
+    sb.append(WORKSPACE_DIRECTORY);
+
+    for(String s: quote.getTags()){
+      sb.append(backslash);
+      sb.append(s);
+    }
+
+    new File(sb.toString()).mkdirs();
+
+    sb.append(backslash);
+    sb.append(filename);
+    sb.append(".utf8");
+
+    //we create the actual file
+    File file = new File(sb.toString());
+    Writer writer = new OutputStreamWriter(new FileOutputStream(file), StandardCharsets.UTF_8);
+    writer.write(quote.getQuote());
+    writer.close();
+
   }
   
   /**
