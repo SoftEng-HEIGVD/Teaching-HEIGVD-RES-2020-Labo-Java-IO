28cdbbf8488a892d5974892cc61dea578d2e54cb
Massaoudi Walid <39918257+WalidMassaoudi@users.noreply.github.com>
2020-03-08 13:01:48 +0100

    explore function added

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,6 +3,7 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
@@ -16,7 +17,26 @@ public class DFSFileExplorer implements IFileExplorer {
 
   @Override
   public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    vistor.visit(rootDirectory);
+    File[] directories = rootDirectory.listFiles(File::isDirectory);
+    File[] files = rootDirectory.listFiles(File::isFile);
+
+    /* chek if root directory exist*/
+    if (!rootDirectory.exists())
+      return;
+
+    if (directories.length != 0) {
+      /* Directories*/
+      Arrays.sort(directories);
+      /* Files */
+      Arrays.sort(files);
+    }
+   /*Dfs vist Directories */
+    for (File dir : directories)
+      explore(dir, vistor);
+    /* DFS vist files*/
+    for (File file : files)
+      vistor.visit(file);
   }
 
 }
