ea146ce0c8f430323b23c7c247ba47098355d8fc
Lindwing <rui.lopesgouveia@heig-vd.ch>
2020-03-08 21:56:05 +0100

    complete utils

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -4,7 +4,7 @@ import java.util.logging.Logger;
 
 /**
  *
- * @author Olivier Liechti
+ * @author Olivier Liechti, modified by Rui Filipe Lopes Gouveia
  */
 public class Utils {
 
@@ -20,7 +20,24 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String[] nextLine = new String[2];
+
+    for (int i = 0; i < lines.length(); ++i) {
+      if ((i + 1) < lines.length() && lines.charAt(i) == '\r' && lines.charAt(i + 1) == '\n') {
+        nextLine[0] = lines.substring(0, i + 2);
+        nextLine[1] = lines.substring(i + 2);
+      }
+      else if (lines.charAt(i) == '\r' || lines.charAt(i) == '\n') {
+        nextLine[0] = lines.substring(0, i + 1);
+        nextLine[1] = lines.substring(i + 1);
+        return nextLine;
+      }
+      else{
+        nextLine[0] = "";
+        nextLine[1] = lines;
+      }
+    }
+    return nextLine;
   }
 
 }
