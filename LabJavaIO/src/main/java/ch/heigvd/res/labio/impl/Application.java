de3ee6c5a3e793163345d68ecc2ef28e26102c40
quillasp <saralfddin93@yahoo.fr>
2020-03-08 18:23:13 +0100

    Application et Utils fonctionnels (les tests passent). Quelques warnings, mais ils ne sont pas de mon ressort.

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,10 +7,8 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -90,6 +88,9 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+
+      storeQuote(quote, String.format("quote-%d", i));
+
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +124,26 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    StringBuilder sbpath = new StringBuilder();
+    sbpath.append(WORKSPACE_DIRECTORY).append("/");
+
+    for (String tag : quote.getTags())
+      sbpath.append(tag).append("/");
+
+    String path = sbpath.toString();
+
+    File f = new File(path);
+    f.mkdirs();
+
+    OutputStreamWriter oswriter = new OutputStreamWriter(
+            new FileOutputStream(path + filename + ".utf8"),
+            "UTF-8"
+    );
+
+    oswriter.write(quote.getQuote());
+    oswriter.flush();
+    oswriter.close();
   }
   
   /**
@@ -140,6 +160,15 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        if (file == null)
+          return;
+
+        try {
+          writer.write(file.getPath());
+          writer.write(System.lineSeparator());
+        } catch (IOException e) {
+          throw new RuntimeException(e);
+        }
       }
     });
   }
--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -11,6 +11,22 @@ public class Utils {
   private static final Logger LOG = Logger.getLogger(Utils.class.getName());
 
   /**
+   * découverte des lookahead et lookbehind grâce à :
+   * - https://docs.oracle.com/javase/8/docs/api/java/util/regex/Pattern.html
+   *   pour ce qui est de la structure d'une regex
+   * - https://www.regular-expressions.info/lookaround.html
+   *   pour l'utilisation des lookahead et lookbehind
+   *   - (?<=X)      : si X se trouve bien avant le caractère
+   *   - (?!X)       : si X ne se trouve pas après le caractère
+   *   - (?<=X(?!Y)) : si X se trouve avant le caractère mais qu'il n'est pas
+   *                   suivi par Y
+   *   - lookahead et lookbehind permettent de faire des comparaisons sur les
+   *     String sans que ceux-ci ne soient consommés : ils permettent de dire
+   *     si le String match ou non la regexe
+   */
+  private static final String REGEX = "(?<=(\r\n))|(?<=\n)|(?<=\r(?!\n))";
+
+  /**
    * This method looks for the next new line separators (\r, \n, \r\n) to extract
    * the next line in the string passed in arguments. 
    * 
@@ -20,7 +36,13 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String[] result = lines.split(REGEX, 2);
+
+    if (result.length == 2)
+      return result;
+    else
+      return new String[]{"", result[0]};
   }
 
 }
