de2c0b6a1b1330ed2b06f0c9bf0b6d2098fb0da2
matt989253 <matthieu.burguburu@gmail.com>
2020-03-10 14:29:57 +0100

    DFSFileExplorer tests pass

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/explorers/DFSFileExplorer.java
@@ -3,20 +3,37 @@ package ch.heigvd.res.labio.impl.explorers;
 import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import java.io.File;
+import java.util.Arrays;
 
 /**
  * This implementation of the IFileExplorer interface performs a depth-first
  * exploration of the file system and invokes the visitor for every encountered
  * node (file and directory). When the explorer reaches a directory, it visits all
  * files in the directory and then moves into the subdirectories.
- * 
+ *
  * @author Olivier Liechti
  */
 public class DFSFileExplorer implements IFileExplorer {
 
   @Override
-  public void explore(File rootDirectory, IFileVisitor vistor) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
-  }
+  public void explore(File rootDirectory, IFileVisitor visitor) {
 
+    visitor.visit(rootDirectory);
+
+    if (!rootDirectory.exists()) {
+      return; // recursion end
+    }
+
+    File[] files = rootDirectory.listFiles(); // all files and folders in root directory
+
+    Arrays.sort(files);
+
+    for (File file : files) {
+      if (file.isFile()) {
+        visitor.visit(file);
+      } else if (file.isDirectory()) { // we don't just use the else, because maybe there is something other than file and directory, just to be sure
+        explore(file, visitor);
+      }
+    }
+  }
 }
