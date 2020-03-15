277d1d81a6f91b3d473ab5e3c4b0f3fed7ac0830
matt989253 <matthieu.burguburu@gmail.com>
2020-03-15 16:33:45 +0100

    Utils tests pass

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Utils.java
@@ -20,7 +20,14 @@ public class Utils {
    * contain any line separator, then the first element is an empty string.
    */
   public static String[] getNextLine(String lines) {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    // using positive look-behind and negative look-ahead regex, explained at https://stackoverflow.com/a/4416576/12199702
+    String[] split = lines.split("(?<=\r\n)|(?<=\n)|(?<=\r)(?!\n)", 2); // need negative look-ahead to not resplit on \r\n
+
+    if (split.length != 2) { // no line separator
+      return new String[] {"", split[0]};
+    }
+
+    return split;
   }
 
 }
