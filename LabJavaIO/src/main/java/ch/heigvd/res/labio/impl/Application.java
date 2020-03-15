7d798676bb4a553402d2aa53bbc36730e176c803
matt989253 <matthieu.burguburu@gmail.com>
2020-03-15 18:35:41 +0100

    Application tests pass

--- LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
+++ LabJavaIO/src/main/java/ch/heigvd/res/labio/impl/Application.java
@@ -7,10 +7,9 @@ import ch.heigvd.res.labio.interfaces.IFileExplorer;
 import ch.heigvd.res.labio.interfaces.IFileVisitor;
 import ch.heigvd.res.labio.quotes.QuoteClient;
 import ch.heigvd.res.labio.quotes.Quote;
-import java.io.File;
-import java.io.IOException;
-import java.io.StringWriter;
-import java.io.Writer;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import org.apache.commons.io.FileUtils;
@@ -90,6 +89,7 @@ public class Application implements IApplication {
        * one method provided by this class, which is responsible for storing the content of the
        * quote in a text file (and for generating the directories based on the tags).
        */
+      storeQuote(quote, "quote-" + i + 1 + ".utf8"); // quotes numbering starts at 1 and not 0
       LOG.info("Received a new joke with " + quote.getTags().size() + " tags.");
       for (String tag : quote.getTags()) {
         LOG.info("> " + tag);
@@ -123,7 +123,18 @@ public class Application implements IApplication {
    * @throws IOException 
    */
   void storeQuote(Quote quote, String filename) throws IOException {
-    throw new UnsupportedOperationException("The student has not implemented this method yet.");
+    String path = WORKSPACE_DIRECTORY + "/";
+
+    for (String tag : quote.getTags()) {
+      path += tag + "/";
+    }
+
+    File dir = new File(path);
+    dir.mkdirs();
+
+    Writer writer = new OutputStreamWriter(new FileOutputStream(path + filename), StandardCharsets.UTF_8);
+    writer.write(quote.getQuote());
+    writer.close();
   }
   
   /**
@@ -140,6 +151,13 @@ public class Application implements IApplication {
          * of the the IFileVisitor interface inline. You just have to add the body of the visit method, which should
          * be pretty easy (we want to write the filename, including the path, to the writer passed in argument).
          */
+        try {
+          writer.write(file.getPath());
+          writer.write("\n");
+        }
+        catch (IOException e) {
+          throw new RuntimeException();
+        }
       }
     });
   }
